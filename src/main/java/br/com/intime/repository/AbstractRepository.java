6f68cc080d79678b39a0283306f1b4b7cc3ac482
Julio Izidoro da Silva Junior <jizidoro@globo.com>
2016-11-09 20:28:22 -0200

    Iniciando projeto

---
+++ src/main/java/br/com/intime/repository/AbstractRepository.java
@@ -0,0 +1,49 @@
+package br.com.intime.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public abstract class AbstractRepository<T> {
+
+    @PersistenceContext
+    private EntityManager em;
+    protected final Class clazz;
+
+    
+    public AbstractRepository(Class clazz) {
+        this.clazz = clazz;
+    }
+
+    public T create(T entity) {
+        em.persist(entity);
+        return entity;
+    }
+
+    public T update(T entity) {
+        return em.merge(entity);
+    }
+
+    public void remove(Integer id) {
+        em.remove(em.getReference(clazz, id));
+    }
+
+    public T find(Integer id) {
+        return (T) em.find(clazz, id);
+    }
+
+    public List<T> list(String sql) {
+        List<T> list = em.createQuery(sql).getResultList();
+        if (list==null){
+            list = new ArrayList<>();
+        }
+        return list;
+    }
+    
+    public T find(String sql) {
+        T t = (T) em.createQuery(sql).getSingleResult();
+        return (T) t;
+    }
+
+}
