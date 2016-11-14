a47b5d657d7f45111b038b837dabcab1d086fbf5
Kamila <Kamila@Kamila-PC>
2016-11-14 19:29:52 -0200

    consulta cliente

--- src/main/java/br/com/intime/managerBean/cliente/ClienteMB.java
+++ src/main/java/br/com/intime/managerBean/cliente/ClienteMB.java
@@ -1,10 +1,16 @@
 package br.com.intime.managerBean.cliente;
- 
-import java.io.Serializable;   
+
+import br.com.intime.model.Cliente;
+import br.com.intime.repository.ClienteRepository;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
-import javax.inject.Named; 
+import javax.inject.Named;
 import org.primefaces.context.RequestContext;
 
 /**
@@ -16,12 +22,42 @@ import org.primefaces.context.RequestContext;
 public class ClienteMB implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    @EJB
+    private ClienteRepository clienteRepository;
+    private List<Cliente> listaCliente;
 
-    public String novoCliente() { 
+    @PostConstruct
+    public void init() {
+        gerarListaCliente();
+    }
+
+    public List<Cliente> getListaCliente() {
+        return listaCliente;
+    }
+
+    public void setListaCliente(List<Cliente> listaCliente) {
+        this.listaCliente = listaCliente;
+    }
+
+    public ClienteRepository getClienteRepository() {
+        return clienteRepository;
+    }
+
+    public void setClienteRepository(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public String novoCliente() {
         Map<String, Object> options = new HashMap<String, Object>();
         options.put("contentWidth", 545);
-        RequestContext.getCurrentInstance().openDialog("cadCliente", options, null); 
+        RequestContext.getCurrentInstance().openDialog("cadCliente", options, null);
         return "";
     }
 
+    public void gerarListaCliente() {
+        listaCliente = clienteRepository.list("Select c from Cliente c");
+        if (listaCliente == null) {
+            listaCliente = new ArrayList<Cliente>();
+        }
+    }
 }
