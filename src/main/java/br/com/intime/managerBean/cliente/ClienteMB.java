5f274d89573a9e666ce250d19ebf15fc87d33714
Kamila <Kamila@Kamila-PC>
2016-11-16 09:04:34 -0200

    upload imagem

--- src/main/java/br/com/intime/managerBean/cliente/ClienteMB.java
+++ src/main/java/br/com/intime/managerBean/cliente/ClienteMB.java
@@ -1,12 +1,19 @@
 package br.com.intime.managerBean.cliente;
 
 import br.com.intime.model.Cliente;
+import br.com.intime.model.Ftpdados;
 import br.com.intime.repository.ClienteRepository;
+import br.com.intime.repository.FtpDadosRepository;
+import br.com.intime.util.Ftp;
+import br.com.intime.util.Mensagem;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
@@ -28,10 +35,14 @@ public class ClienteMB implements Serializable {
     private ClienteRepository clienteRepository;
     private List<Cliente> listaCliente;
     private String nomefantasia;
+    private Ftpdados ftpdados;
+    @EJB
+    private FtpDadosRepository ftpDadosRepository;
 
     @PostConstruct
     public void init() {
         gerarListaCliente();
+        ftpdados = ftpDadosRepository.find("select f from Ftpdados f"); 
     }
 
     public List<Cliente> getListaCliente() {
@@ -56,6 +67,22 @@ public class ClienteMB implements Serializable {
 
     public void setNomefantasia(String nomefantasia) {
         this.nomefantasia = nomefantasia;
+    }
+
+    public Ftpdados getFtpdados() {
+        return ftpdados;
+    }
+
+    public void setFtpdados(Ftpdados ftpdados) {
+        this.ftpdados = ftpdados;
+    }
+
+    public FtpDadosRepository getFtpDadosRepository() {
+        return ftpDadosRepository;
+    }
+
+    public void setFtpDadosRepository(FtpDadosRepository ftpDadosRepository) {
+        this.ftpDadosRepository = ftpDadosRepository;
     }
 
     public String novoCliente() {
@@ -87,5 +114,11 @@ public class ClienteMB implements Serializable {
         if (listaCliente == null) {
             listaCliente = new ArrayList<Cliente>();
         }
+    }
+    
+    public boolean possuiImagem(Cliente cliente){
+        if(cliente.getNomefoto()!=null && cliente.getNomefoto().length()>0){
+            return true;
+        }else return false;
     }
 }
